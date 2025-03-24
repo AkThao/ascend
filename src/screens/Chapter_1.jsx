2376fc9ecec02e264041070029710d5f52ababd4
AkThao <akaash.thao@gmail.com>
2025-03-24 22:17:33 +0000

    Clean up styles and add capability for multiple lines of text

--- src/screens/Chapter_1.jsx
+++ src/screens/Chapter_1.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Chapter1 = () => {
+    const allStorylines = [
+        "I’ve been here as long as I can remember",
+        "In fact, this is all I’ve ever known, all any of us have ever known"
+    ];
+    const [currStorylines, setCurrStorylines] = useState([]);
+
+    const handleNextLine = () => {
+        if (currStorylines.length < allStorylines.length) {
+            setCurrStorylines([...currStorylines, allStorylines[currStorylines.length]]);
+        }
+    };
+
     return (
         <>
-            <h1>Chapter 1: Look to the Sky</h1>
-            <p>[The beginning of the story]</p>
+            <div
+                onClick={handleNextLine}
+                style={{
+                    height: "100vh",
+                    width: "100vw",
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    fontFamily: "monospace"
+                }}
+            >
+                <h1>Chapter 1: Look to the Sky</h1>
+                {currStorylines.map((line, idx) => (
+                    <p key={idx}>{line}</p>
+                ))}
+            </div>
         </>
     );
 };
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,8 +3,25 @@ import React from "react";
 const Home = ({ onContinue }) => {
     return (
         <>
-            <h1>Ascend</h1>
-            <p onClick={onContinue} style={{ cursor: "pointer" }}>Click to continue...</p>
+            <h1
+                style={{
+                    fontFamily: "monospace",
+                    fontSize: "60px",
+                    userSelect: "none"
+                }}
+            >
+                Ascend
+            </h1>
+            <p
+                onClick={onContinue}
+                style={{
+                    cursor: "pointer",
+                    fontFamily: "monospace",
+                    userSelect: "none"
+                }}
+            >
+                Click to continue...
+            </p>
         </>
     );
 };
